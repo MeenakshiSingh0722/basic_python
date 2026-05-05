290a9841305e700c8a536066ab2b9b8ae092c37c
Meenakshi Singh <meenakshisingh0722@gmail.com>
2026-05-05 23:55:35 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,7 @@
+set1={1,2,3,4,2,4} #store unique value
+print(set1)
+print(type(set1))
+print(len(set1))
+
+null_set=set() #empty set 
+print(null_set)
---
+++ setMethod.py
@@ -0,0 +1,12 @@
+a=set()
+a.add(1) #add new
+a.add(2)
+a.add(4) #{1,2,4}
+a.remove(4) #{1,2}invalid pr error 
+a.clear()
+print(a)
+
+b={1,2,3,4}
+c={3,4,5,6}
+print(b.union(c))
+print(b.intersection(c))
